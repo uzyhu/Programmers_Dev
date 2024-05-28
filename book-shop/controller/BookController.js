3e3654e65e2d2ceae9e61942d713d6657729db97
uzyhu <dnwlgn0807@naver.com>
2024-05-28 22:00:50 +0900

    add:table join & limit, offset & date_sub(between)

--- book-shop/controller/BookController.js
+++ book-shop/controller/BookController.js
@@ -1,44 +1,51 @@
 const conn = require('../mariadb'); //db 모듈
 const { StatusCodes } = require('http-status-codes'); //status code 모듈
 
-
+//(카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-    let { category_id } = req.query; 
+    let { category_id, news, limit, currentPage } = req.query;
 
-    if (category_id) { //카테고리별 도서 목록 조회 (쿼리스트링으로 받을거임)
-        let sql = 'SELECT * FROM books WHERE category_id=?';
-        conn.query(sql, category_id,
-            (err, results) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-                if (results.length) {
-                    return res.status(StatusCodes.OK).json(results);
-                }
-                else {
-                    return res.status(StatusCodes.NOT_FOUND).end();
-                }
-            })
+    //limit : page 당 도서 수     ex.3
+    //currentPage : 현재 몇 페이지 ex. 1,2,3...
+    //offset :                  ex. 0,3,6,9... limit*(currentPage-1)
+    //카테고리별 도서 목록 조회 (쿼리스트링으로 받을거임)
+    let offset = limit * (currentPage-1); //계산해서 자동으로 숫자로 계산됨
+    let sql = 'SELECT * FROM books';
+    let values = [];
+    if(category_id && news) {
+        sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+        values = [category_id];
     }
-    else { //전체도서조회, (요약된 전체 도서 조회)
-        let sql = 'SELECT * FROM books';
-        conn.query(sql,
-            (err, results) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
+    else if(category_id) {
+        sql += " WHERE category_id = ?";
+        values = [category_id];
+    }
+    else if(news) {
+        sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    }
 
+    sql += " LIMIT ? OFFSET ?"
+    values.push(parseInt(limit), offset);
+
+    conn.query(sql, values,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if (results.length) {
                 return res.status(StatusCodes.OK).json(results);
-            })
-    }
+            }
+            else {
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+        })
 }
 
 const bookDetail = (req, res) => {//개별도서조회
     let { id } = req.params;
 
-    let sql = 'SELECT * FROM books WHERE id=?';
+    let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`;
     conn.query(sql, id,
         (err, results) => {
             if (err) {
