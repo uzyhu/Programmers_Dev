d14d6275aaa2f7f6e608cf4fd56a80abc3686e83
uzyhu <dnwlgn0807@naver.com>
2024-07-26 11:27:03 +0900

    add: firebase connect & use auth func

--- react-task-app/src/components/BoardList/BoardList.tsx
+++ react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,42 +1,94 @@
 import React, { FC, useRef, useState } from "react";
-import { useTypedSelector } from "../../hooks/redux";
+import { useTypedDispatch, useTypedSelector } from "../../hooks/redux";
 import SideForm from "./SideForm/SideForm";
-import { FiPlusCircle } from "react-icons/fi";
-import { addButton, addSection, boardItem, boardItemActive, container, title } from "./BoardList.css";
+import { FiLogIn, FiPlusCircle } from "react-icons/fi";
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from "./BoardList.css";
 import clsx from "clsx";
+import { GoSignOut } from "react-icons/go";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { app } from "../../firebase";
+import { removeUser, setUser } from "../../store/slices/userSlice";
+import { useAuth } from "../../hooks/useAuth";
 
 type TBoardListProps = {
   activeBoardId: string;
   setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId}) => {
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const dispatch = useTypedDispatch();
   const { boardArray } = useTypedSelector((state) => state.boards);
   const [isFormOpen, setIsFormOpen] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const {isAuth} = useAuth();
+
+  const handleLogin = () => {
+    signInWithPopup(auth, provider).then((userCredential) => {
+      dispatch(
+        setUser({
+          email: userCredential.user.email,
+          id: userCredential.user.uid
+        })
+      )
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  };
 
   const handleClick = () => {
     setIsFormOpen(!isFormOpen);
     setTimeout(() => {
       inputRef.current?.focus();
     }, 0);
-  }
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      dispatch(
+        removeUser()
+      )
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  };
+
   return (
     <div className={container}>
       <div className={title}>게시판:</div>
       {boardArray.map((board, index) => (
-        <div key={board.boardId}
-        onClick={() => setActiveBoardId(boardArray[index].boardId)}
-        className= {
-          clsx({
-            [boardItemActive] :
-            boardArray.findIndex(b => b.boardId === activeBoardId) === index
-          },
-          {
-            [boardItem] :
-            boardArray.findIndex(b => b.boardId === activeBoardId) !== index
-          })
-        }>
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)}
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) ===
+                index,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                index,
+            }
+          )}
+        >
           <div>{board.boardName}</div>
         </div>
       ))}
@@ -49,6 +101,13 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId}) => {
             onClick={handleClick}
           ></FiPlusCircle>
         )}
+        {
+          isAuth
+          ?
+          <GoSignOut className={addButton} onClick={handleSignOut}/>
+          :
+          <FiLogIn className={addButton} onClick={handleLogin} />
+        }
       </div>
     </div>
   );
---
+++ react-task-app/src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9ErhLx1E3zl6Mzsu4uMWa2zvtWEVbd1s",
+  authDomain: "react-task-app-44400.firebaseapp.com",
+  projectId: "react-task-app-44400",
+  storageBucket: "react-task-app-44400.appspot.com",
+  messagingSenderId: "445454917810",
+  appId: "1:445454917810:web:f3c7d6d9a0b496065a69ab"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+//파이어베이스와 연결
